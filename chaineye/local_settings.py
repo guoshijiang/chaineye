cb4c293c326dedca98f23de2e8e8de747fba4e1d
BJ89182ML <bj89182ml@192.168.0.103>
2022-03-06 21:16:48 +0800

    fix

--- chaineye/local_settings.py
+++ chaineye/local_settings.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chaineye',
         'USER': 'root',
-        'PASSWORD': 'Zhifish2020!',
+        'PASSWORD': 'Zhiyu2019!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
@@ -11,3 +11,21 @@ DATABASES = {
         },
     }
 }
+
+#配置Redis为Django缓存
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+ACCESSKEYID = 'LTAI4FsrVizgQCehFNZGBdJU'   # 短信验证吗 accessKeyId
+ACCESSSECRET = 'prVvf6xYJoYTl99YZAlmbqBnZ6OaTG'  # 短信验证码 accessSecret
+SIGNNAME = '问我学院'      # 短信验证码 SignName
+TEMPLATECODE = 'SMS_183765549'  # 短信验证码 TemplateCode
+
+
