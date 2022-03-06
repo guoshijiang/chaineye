99107e71c9743082fea78d452dde1f2eacb49fda
BJ89182ML <bj89182ml@192.168.0.103>
2022-03-06 21:20:32 +0800

    fix

--- chaineye/settings.py
+++ chaineye/settings.py
@@ -65,12 +65,23 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chaineye',
         'USER': 'root',
-        'PASSWORD': 'Wenwo2020!',
+        'PASSWORD': 'Zhiyu2019!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
+    }
+}
+
+#配置Redis为Django缓存
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 
@@ -169,10 +180,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 API_TOKEN = 'WenwoChaineyeChannel'
-ACCESSKEYID = ''   # 短信验证吗 accessKeyId
-ACCESSSECRET = ''  # 短信验证码 accessSecret
-SIGNNAME = ''      # 短信验证码 SignName
-TEMPLATECODE = ''  # 短信验证码 TemplateCode
+ACCESSKEYID = 'LTAI4FsrVizgQCehFNZGBdJU'   # 短信验证吗 accessKeyId
+ACCESSSECRET = 'prVvf6xYJoYTl99YZAlmbqBnZ6OaTG'  # 短信验证码 accessSecret
+SIGNNAME = '问我学院'      # 短信验证码 SignName
+TEMPLATECODE = 'SMS_183765549'  # 短信验证码 TemplateCode
 
 
 try:
